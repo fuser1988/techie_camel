79d83a4c881a71ee289f21013ab4a472a8e8791f
fuser1988 <sergioluke35@gmail.com>
2019-10-15 04:52:34 -0300

    flujo de un mensaje

--- src/main/java/ar/pnt/techie_camel/MyRouteBuilder.java
+++ src/main/java/ar/pnt/techie_camel/MyRouteBuilder.java
@@ -16,7 +16,16 @@ public class MyRouteBuilder extends RouteBuilder {
         // based routing on the message using method references
 
         from("timer:simple?period=1000")
-        .log("procesando mensage")
-        .end();        
+        .log("Disparando procesamiento")
+        .setHeader("Cabecera1",constant("valor cabecera1"))
+        .setBody(constant("Mensaje a procesar"))
+        .to("direct:procesarMensaje")
+        .end();
+
+        from("direct:procesarMensaje")
+        .log("Inicio procesamiento de mensaje")
+        .log("Body = ${body} , cabecera1 = ${header.cabecera1}")
+        .end();
+        
     }
 }
